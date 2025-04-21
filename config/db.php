c923b761c0d5144754cd51ebfaa149e84a9a61b0
Bang Zul Azmi <133740404+Azmi2511@users.noreply.github.com>
2025-04-21 19:59:43 +0700

    Add files via upload

--- config/db.php
+++ config/db.php
@@ -1,8 +1,13 @@
 <?php
-$host = "localhost";
-$user = "root";
-$pass = "";
-$db   = "absensiqrcode";
+$host = "sql12.freesqldatabase.com";
+$user = "sql12774483";
+$pass = "fVjC6MAcCg";
+$db   = "sql12774483";
+// <?php
+// $host = "localhost";
+// $user = "root";
+// $pass = "";
+// $db   = "absensiqrcode";
 
 $conn = new mysqli($host, $user, $pass, $db);
 if ($conn->connect_error) {
